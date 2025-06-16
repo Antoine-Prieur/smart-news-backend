abaf7605ecbe9b54af0fb38290ff81be7f699195
Antoine Prieur <antoine.prieur45@gmail.com>
2025-06-16 04:11:13 -0400

    fix: update CORS

--- src/web/routes.rs
+++ src/web/routes.rs
@@ -8,9 +8,7 @@ pub fn create_router(repository: ArticleRepository) -> Router {
     let cors = CorsLayer::new()
         .allow_origin([
             "http://localhost:3000".parse().unwrap(),
-            "https://smart-news-frontend-nd4yy2bea-antoine-prieurs-projects.vercel.app"
-                .parse()
-                .unwrap(),
+            "https://smart-news-frontend.vercel.app/".parse().unwrap(),
         ])
         .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
         .allow_headers(Any);
